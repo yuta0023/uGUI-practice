782cfbb3048839581d01e86c17bf05a62710dedd
yuta.y <mkapp.4smile.23@gmail.com>
2015-12-26 00:33:16 +0900

    First Commit

---
+++ Assets/NobelTest/TextController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+	
+	public string[] scenarios;
+	public Text uiText;
+	
+	int currentLine = 0;
+	
+	void Start()
+	{
+		TextUpdate();
+	}
+	
+	void Update () 
+	{
+		if(currentLine < scenarios.Length && Input.GetMouseButtonDown(0))
+		{
+			TextUpdate();
+		}
+	}
+	
+
+	void TextUpdate()
+	{
+		uiText.text = scenarios[currentLine];
+		currentLine ++;
+	}
+}
